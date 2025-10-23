676e2f867c4b189379e0a9e31dc85eb13e274ad0
SohanFlick <sohan.flick@gmail.com>
2025-10-23 20:55:01 +0600

    practice set

---
+++ CHAPTER-2 -PS/ps-4.py
@@ -0,0 +1,6 @@
+a = int(input("Enter your number a:"))
+b = int(input("Enter your number b:"))
+
+c = a > b
+
+print("a is greater than b:", c)
---
+++ CHAPTER-2 -PS/ps-6.py
@@ -0,0 +1,3 @@
+a = int(input("Enter your number a:"))
+
+print("the square of a number entered by the user is:", (a))
