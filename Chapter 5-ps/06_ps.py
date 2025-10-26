eeb09a8cf13f60e9d2e637f8377288bd050d2ba0
jahid-hassan <jahidhassan9006@gmail.com>
2025-10-27 01:01:32 +0600

    cp 5

--- Chapter 5-ps/06_ps.py
+++ Chapter 5-ps/06_ps.py
@@ -1,19 +1,31 @@
 # Create an empty dictionary. Allow 4 friends to enter their favorite language as
 # value and use key as their names. Assume that the names are unique.
 
+d = {}
 
-e_lang = {}
+# lang = {
+#     "sohan": "bangla",
+#     "rohan": "english",
+#     "boni": "china",
+#     "foisal": "hinde",
+# }
 
-lang = {
-    "sohan": "bangla",
-    "rohan": "english",
-    "boni": "china",
-    "foisal": "hinde",
-}
+name = input('Enter your name:')
+lang = input('Enter your lang:')
 
-obj = input("Enter your favorite language:")
+d.update({name: lang})
+name = input('Enter your name:')
+lang = input('Enter your lang:')
 
-e_lang.
+d.update({name: lang})
+name = input('Enter your name:')
+lang = input('Enter your lang:')
+
+d.update({name: lang})
+name = input('Enter your name:')
+lang = input('Enter your lang:')
+
+d.update({name: lang})
 
 
-print(lang[obj])
+print(d)  # {'Rakib': 'bangla', 'raju ': 'english', 'priya': 'hinde', 'royal': 'urdu'}
---
+++ Chapter 5-ps/08ps..py
@@ -0,0 +1,5 @@
+# If languages of two friends are same; what will happen to the program in problem
+# 6?
+
+
+# ans: nothing happen
