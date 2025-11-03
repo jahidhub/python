d99833feda9d48f6849ade29c54ec1b913e43691
SohanFlick <sohan.flick@gmail.com>
2025-11-03 20:51:21 +0600

    null

---
+++ Chapter 9 - File/01_read.py
@@ -0,0 +1,20 @@
+# f = open("myFile.txt")
+# print(f.read())
+# f.close()
+
+
+# with open("myFile.txt") as f:
+#     print(f.read())
+# with open("myFile.txt") as f:
+#     print(f.read(5))
+    
+# with open("myFile.txt") as f:
+#     print(f.readline())
+    
+with open("myFile.txt") as f:
+    print(f.readlines())
+    
+    
+# with open("myFile.txt") as f:
+#     for x in f:
+#         print(x)
