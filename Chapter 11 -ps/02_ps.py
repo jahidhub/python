260559b38b78beaa8369a5a94ba1b382b3eabf59
jahid-hassan <jahidhassan9006@gmail.com>
2025-11-09 20:29:27 +0600

    chapter 11 ps

---
+++ Chapter 11 -ps/02_ps.py
@@ -0,0 +1,23 @@
+"""Create a class ‘Pets’ from a class ‘Animals’ and further create a class ‘Dog’ from
+‘Pets’. Add a method ‘bark’ to class ‘Dog’.
+"""
+
+
+class Animals:
+    pass
+
+
+class Pets(Animals):
+    pass
+
+
+class Dog(Pets):
+    
+    @staticmethod
+    def bark():
+        print("bow bow")
+
+
+d = Dog()
+
+d.bark()
