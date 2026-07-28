db6b4ebb5f583a927a68de4f789961d8fe7902b9
Jahid Hassan <jahidhassan9006@gmail.com>
2026-07-28 21:02:20 +0600

    data-type.py

--- yahoo/data-type.py
+++ yahoo/data-type.py
@@ -1,3 +1,12 @@
 x = ("Hello", 25, "Sohan")
 
 print(type(x))
+
+
+a = 20
+b= 15
+
+if a<=b:
+    print("this is true")
+else:
+    print("Not true")
---
+++ yahoo/if-elif-else.py
@@ -0,0 +1,22 @@
+# score = int(input("Enter Your Name: "))
+
+# if score >= 80:
+#     print("Grade: A+")
+# elif score>=70:
+#     print("Grade: A")
+# elif score >= 65:
+#     print("Grade: A-")
+# elif score >= 60:
+#     print("Grade: B")
+# elif score >= 50:
+#     print("Grade: C")
+# elif score >= 34:
+#     print("Grade: D")
+# else:
+#     print("Grade: Fail")
+
+
+a = 100
+b= 200
+
+print("Hello this is first") if a > b else print("This is end")
